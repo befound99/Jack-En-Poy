7c5236135c3d43a3f6924a73d8efe46f0f14fc89
befound99 <jayvievievie@gmail.com>
2023-04-13 10:49:58 +0800

    added  some animation and history

--- assets/script/index-script.js
+++ assets/script/index-script.js
@@ -36,7 +36,10 @@
 // Match result container
   let matchResult = document.querySelector("#result");
 
-// user chose rock funtion
+// history div
+  let matchHistory = document.querySelector(`#matchHistory`);
+
+// user chose rock function
 function chooseRock () {
   // ai choice randomizer where : 0 = rock, 1 = paper, 2 = scissors
   let aiRandom = Math.floor((Math.random() * 3));
@@ -50,15 +53,42 @@ function chooseRock () {
   userDisplayScissors.setAttribute('style', 'display: none !important;');
 
   if (aiRandom == 0) {
+    
 
     aiDisplayRock.setAttribute('style', 'display: block !important;');
     aiDisplayPaper.setAttribute('style', 'display: none !important;');
     aiDisplayScissors.setAttribute('style', 'display: none !important;');
 
     matchResult.innerHTML = "DRAW";
-    matchResult.style.textShadow = "0 0 5px #5ED974"
-    matchResult.style.color = "#5ED974"
+    matchResult.style.textShadow = "0 0 5px #5ED974";
+    matchResult.style.color = "#5ED974";
+
+    // create element
+    let loseHistory = document.createElement(`div`);
+    let loseHistoryPlayerPick = document.createElement(`div`);
+    let loseHistoryResult = document.createElement(`div`)
+    let loseHistoryAIPick = document.createElement(`div`);
+
+    // append
+    matchHistory.appendChild(loseHistory);
+      // history-draw content
+      loseHistory.appendChild(loseHistoryPlayerPick);
+      loseHistory.appendChild(loseHistoryResult);
+      loseHistory.appendChild(loseHistoryAIPick);
+
+    // div attrributes
+    loseHistory.classList.add(`history-draw`);
+
+    // div content
+    loseHistoryPlayerPick.innerHTML = `<i class="fa-solid fa-hand-back-fist history-icon-size"></i>`;
+    loseHistoryResult.innerHTML = `<p class="history-draw-message">DRAW</p>`;
+    loseHistoryAIPick.innerHTML = `<i class="fa-solid fa-hand-back-fist history-icon-size"></i>`
+
+    
+
+
     return ;
+
   }else if (aiRandom == 1) {
 
     aiDisplayPaper.setAttribute('style', 'display: block !important;');
@@ -66,11 +96,32 @@ function chooseRock () {
     aiDisplayScissors.setAttribute('style', 'display: none !important;');
 
     matchResult.innerHTML = "YOU LOSE";
-    matchResult.style.textShadow = "0 0 5px #f04343"
-    matchResult.style.color = "#f04343"
+    matchResult.style.textShadow = "0 0 5px #f04343";
+    matchResult.style.color = "#f04343";
     aiScoreValue++;
     
-    return;
+    // create element
+    let loseHistory = document.createElement(`div`);
+    let loseHistoryPlayerPick = document.createElement(`div`);
+    let loseHistoryResult = document.createElement(`div`)
+    let loseHistoryAIPick = document.createElement(`div`);
+
+    // append
+    matchHistory.appendChild(loseHistory);
+      // history-draw content
+      loseHistory.appendChild(loseHistoryPlayerPick);
+      loseHistory.appendChild(loseHistoryResult);
+      loseHistory.appendChild(loseHistoryAIPick);
+
+    // div attrributes
+    loseHistory.classList.add(`history-lose`);
+
+    // div content
+    loseHistoryPlayerPick.innerHTML = `<i class="fa-solid fa-hand-back-fist history-icon-size"></i>`;
+    loseHistoryResult.innerHTML = `<p class="history-lose-message">LOSE</p>`;
+    loseHistoryAIPick.innerHTML = `<i class="fa-solid fa-hand history-icon-size"></i>`
+
+
   }else if (aiRandom == 2) {
 
     aiDisplayScissors.setAttribute('style', 'display: block !important;');
@@ -78,10 +129,32 @@ function chooseRock () {
     aiDisplayRock.setAttribute('style', 'display: none !important;');
 
     matchResult.innerHTML = "YOU WIN!";
-    matchResult.style.textShadow = "0 0 5px #5549fd"
-    matchResult.style.color = "#5549fd"
+    matchResult.style.textShadow = "0 0 5px #5549fd";
+    matchResult.style.color = "#5549fd";
     userScoreValue++;
-    return; 
+
+    // create element
+    let loseHistory = document.createElement(`div`);
+    let loseHistoryPlayerPick = document.createElement(`div`);
+    let loseHistoryResult = document.createElement(`div`)
+    let loseHistoryAIPick = document.createElement(`div`);
+
+    // append
+    matchHistory.appendChild(loseHistory);
+      // history-draw content
+      loseHistory.appendChild(loseHistoryPlayerPick);
+      loseHistory.appendChild(loseHistoryResult);
+      loseHistory.appendChild(loseHistoryAIPick);
+
+    // div attrributes
+    loseHistory.classList.add(`history-win`);
+
+    // div content
+    loseHistoryPlayerPick.innerHTML = `<i class="fa-solid fa-hand-back-fist history-icon-size"></i>`;
+    loseHistoryResult.innerHTML = `<p class="history-win-message">WIN</p>`;
+    loseHistoryAIPick.innerHTML = `<i class="fa-solid fa-hand-scissors  fa-rotate-90 history-icon-size"></i>`
+
+     
   } 
 }
 function choosePaper (){
@@ -105,6 +178,28 @@ function choosePaper (){
     matchResult.innerHTML = "DRAW";
     matchResult.style.textShadow = "0 0 5px #5ED974"
     matchResult.style.color = "#5ED974"
+
+    // create element
+    let loseHistory = document.createElement(`div`);
+    let loseHistoryPlayerPick = document.createElement(`div`);
+    let loseHistoryResult = document.createElement(`div`)
+    let loseHistoryAIPick = document.createElement(`div`);
+
+    // append
+    matchHistory.appendChild(loseHistory);
+      // history-draw content
+      loseHistory.appendChild(loseHistoryPlayerPick);
+      loseHistory.appendChild(loseHistoryResult);
+      loseHistory.appendChild(loseHistoryAIPick);
+
+    // div attrributes
+    loseHistory.classList.add(`history-draw`);
+
+    // div content
+    loseHistoryPlayerPick.innerHTML = `<i class="fa-solid fa-hand history-icon-size"></i>`;
+    loseHistoryResult.innerHTML = `<p class="history-draw-message">DRAW</p>`;
+    loseHistoryAIPick.innerHTML = `<i class="fa-solid fa-hand history-icon-size"></i>`
+    
     return ;
   }else if (aiRandom == 2) {
 
@@ -117,7 +212,28 @@ function choosePaper (){
     matchResult.style.color = "#f04343"
     aiScoreValue++;
     
-    return;
+    // create element
+    let loseHistory = document.createElement(`div`);
+    let loseHistoryPlayerPick = document.createElement(`div`);
+    let loseHistoryResult = document.createElement(`div`)
+    let loseHistoryAIPick = document.createElement(`div`);
+
+    // append
+    matchHistory.appendChild(loseHistory);
+      // history-draw content
+      loseHistory.appendChild(loseHistoryPlayerPick);
+      loseHistory.appendChild(loseHistoryResult);
+      loseHistory.appendChild(loseHistoryAIPick);
+
+    // div attrributes
+    loseHistory.classList.add(`history-lose`);
+
+    // div content
+    loseHistoryPlayerPick.innerHTML = `<i class="fa-solid fa-hand history-icon-size"></i>`;
+    loseHistoryResult.innerHTML = `<p class="history-lose-message">LOSE</p>`;
+    loseHistoryAIPick.innerHTML = `<i class="fa-solid fa-hand-scissors  fa-rotate-90 history-icon-size"></i>`
+
+    
   }else if (aiRandom == 0) {
 
     aiDisplayRock.setAttribute('style', 'display: block !important;');
@@ -128,10 +244,33 @@ function choosePaper (){
     matchResult.style.textShadow = "0 0 5px #5549fd"
     matchResult.style.color = "#5549fd"
     userScoreValue++;
-    return; 
+
+    // create element
+    let loseHistory = document.createElement(`div`);
+    let loseHistoryPlayerPick = document.createElement(`div`);
+    let loseHistoryResult = document.createElement(`div`)
+    let loseHistoryAIPick = document.createElement(`div`);
+
+    // append
+    matchHistory.appendChild(loseHistory);
+      // history-draw content
+      loseHistory.appendChild(loseHistoryPlayerPick);
+      loseHistory.appendChild(loseHistoryResult);
+      loseHistory.appendChild(loseHistoryAIPick);
+
+    // div attrributes
+    loseHistory.classList.add(`history-win`);
+
+    // div content
+    loseHistoryPlayerPick.innerHTML = `<i class="fa-solid fa-hand history-icon-size"></i>`;
+    loseHistoryResult.innerHTML = `<p class="history-win-message">WIN</p>`;
+    loseHistoryAIPick.innerHTML = `<i class="fa-solid fa-hand-back-fist history-icon-size"></i>`
+
+     
   } 
 }
 function chooseScissors () {
+  
   // ai choice randomizer where : 0 = rock, 1 = paper, 2 = scissors
   let aiRandom = Math.floor((Math.random() * 3));
   
@@ -152,6 +291,28 @@ function chooseScissors () {
     matchResult.innerHTML = "DRAW";
     matchResult.style.textShadow = "0 0 5px #5ED974"
     matchResult.style.color = "#5ED974"
+
+    // create element
+    let loseHistory = document.createElement(`div`);
+    let loseHistoryPlayerPick = document.createElement(`div`);
+    let loseHistoryResult = document.createElement(`div`)
+    let loseHistoryAIPick = document.createElement(`div`);
+
+    // append
+    matchHistory.appendChild(loseHistory);
+      // history-draw content
+      loseHistory.appendChild(loseHistoryPlayerPick);
+      loseHistory.appendChild(loseHistoryResult);
+      loseHistory.appendChild(loseHistoryAIPick);
+
+    // div attrributes
+    loseHistory.classList.add(`history-draw`);
+
+    // div content
+    loseHistoryPlayerPick.innerHTML = `<i class="fa-solid fa-hand-scissors  fa-rotate-90 history-icon-size"></i>`;
+    loseHistoryResult.innerHTML = `<p class="history-draw-message">DRAW</p>`;
+    loseHistoryAIPick.innerHTML = `<i class="fa-solid fa-hand-scissors  fa-rotate-90 history-icon-size"></i>`
+
     return ;
   }else if (aiRandom == 0) {
 
@@ -163,19 +324,62 @@ function chooseScissors () {
     matchResult.style.textShadow = "0 0 5px #f04343"
     matchResult.style.color = "#f04343"
     aiScoreValue++;
+
+    // create element
+    let loseHistory = document.createElement(`div`);
+    let loseHistoryPlayerPick = document.createElement(`div`);
+    let loseHistoryResult = document.createElement(`div`)
+    let loseHistoryAIPick = document.createElement(`div`);
+
+    // append
+    matchHistory.appendChild(loseHistory);
+      // history-draw content
+      loseHistory.appendChild(loseHistoryPlayerPick);
+      loseHistory.appendChild(loseHistoryResult);
+      loseHistory.appendChild(loseHistoryAIPick);
+
+    // div attrributes
+    loseHistory.classList.add(`history-lose`);
+
+    // div content
+    loseHistoryPlayerPick.innerHTML = `<i class="fa-solid fa-hand-scissors  fa-rotate-90 history-icon-size"></i>`;
+    loseHistoryResult.innerHTML = `<p class="history-lose-message">LOSE</p>`;
+    loseHistoryAIPick.innerHTML = `<i class="fa-solid fa-hand-back-fist history-icon-size"></i>`
     
-    return;
+    
   }else if (aiRandom == 1) {
+    
+    aiDisplayPaper.setAttribute('style', 'display: block !important;'); 
+    aiDisplayRock.setAttribute('style', 'display: none !important;'); 
+    aiDisplayScissors.setAttribute('style', 'display: none !important;'); 
 
-    aiDisplayPaper.setAttribute('style', 'display: block !important;');
-    aiDisplayRock.setAttribute('style', 'display: none !important;');
-    aiDisplayScissors.setAttribute('style', 'display: none !important;');
+    matchResult.innerHTML = "YOU WIN!"; 
+    matchResult.style.textShadow = "0 0 5px #5549fd"; 
+    matchResult.style.color = "#5549fd"; 
+    userScoreValue++; 
 
-    matchResult.innerHTML = "YOU WIN!";
-    matchResult.style.textShadow = "0 0 5px #5549fd"
-    matchResult.style.color = "#5549fd"
-    userScoreValue++;
-    return; 
+    // create element
+    let loseHistory = document.createElement(`div`);
+    let loseHistoryPlayerPick = document.createElement(`div`);
+    let loseHistoryResult = document.createElement(`div`)
+    let loseHistoryAIPick = document.createElement(`div`);
+
+    // append
+    matchHistory.appendChild(loseHistory);
+      // history-draw content
+      loseHistory.appendChild(loseHistoryPlayerPick);
+      loseHistory.appendChild(loseHistoryResult);
+      loseHistory.appendChild(loseHistoryAIPick);
+
+    // div attrributes
+    loseHistory.classList.add(`history-win`);
+
+    // div content
+    loseHistoryPlayerPick.innerHTML = `<i class="fa-solid fa-hand-scissors  fa-rotate-90 history-icon-size"></i>`;
+    loseHistoryResult.innerHTML = `<p class="history-win-message">WIN</p>`;
+    loseHistoryAIPick.innerHTML = `<i class="fa-solid fa-hand history-icon-size"></i>`
+
+     
   } 
 }
 function reloadPage(){
